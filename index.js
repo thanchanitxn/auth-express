e747e80f04bac76f558e1af9a9e91d01d1e447bd
thanchanitxn <pluem258zx@gmail.com>
2025-03-23 17:48:02 +0700

    update

--- index.js
+++ index.js
@@ -42,19 +42,99 @@ const initMySQL = async () => {
 
 /* เราจะแก้ไข code ที่อยู่ตรงกลาง */
 
-app.post('/api/register', async (req, res) => {
-  const { email, password } = req.body;
+app.post('/api/register', async (req, res) => { 
+  try{const { email, password } = req.body;
+  const passswordHash = await bcrypt.hash(password, 10);
   const userData = {
     email,
-    password
+    password: passswordHash
   }
   const [results] = await conn.query('INSERT INTO users SET ?', userData);
   res.json({ 
     message: 'Register success',
     results
-  })
+  })}catch(error){
+    console.log('error',error)
+    res.json({
+      message: 'insert failed',
+      error
+    })
+  }
+  
 });
 
+app.post ('/api/login', async (req, res) => {
+  try{const { email, password } = req.body;
+  const [results] = await conn.query('SELECT * from users where email = ?', [email]);
+  const userData = results[0];
+  const match = await bcrypt.compare(password, userData.password)
+  if(!match){
+    res.status(400).json({
+      message: 'login failed wrong email or password'
+  })
+  return false
+  }
+/*
+  //สร้าง token
+  const token = jwt.sign({ email,role:'admin' }, secret, { expiresIn: '1h' });
+  res.cookie('token',token,{
+    maxAge:300000,
+    secure:true,
+    httpOnly:true,
+    sameSite:'None'
+  })
+
+*/
+  req.session.userId = userData.id;
+  req.session.user = userData;
+
+  res.json({ 
+    message: 'login success',
+  })
+}catch(error){
+  console.log('error',error)
+  res.status(401).json({
+    message: 'login failed',
+    error
+  })
+}
+})
+
+app.get('/api/users', async (req, res) => {
+  try{/*
+    const authToken = req.cookies.token;
+    // const authHeader = req.headers['authorization'];
+    // let authToken=''
+    // if(authHeader){
+    //   authToken = authHeader.split(' ')[1]
+    // }
+    console.log('authToken',authToken)
+    const user = jwt.verify(authToken, secret);
+    console.log('user',user)
+*/
+    if(!req.session.userId){
+      throw {message: 'Auth fail'}
+    }
+    console.log('req.session',req.session)
+    console.log(req.sessionID)
+    /*
+    const [checkResults] = await conn.query('SELECT * from users WHERE email = ?', [user.email]);
+    if(!checkResults[0]){
+      throw {message: 'user not found'}
+    }*/
+    const [results] = await conn.query('SELECT * from users');
+    res.json({ 
+      users:results[0]
+    })
+
+  }catch(error){
+    console.log('error',error)
+  res.status(403).json({
+    message: 'Authentication failed',
+    error
+  })
+  }
+})
 // Listen
 app.listen(port, async () => {
   await initMySQL();
